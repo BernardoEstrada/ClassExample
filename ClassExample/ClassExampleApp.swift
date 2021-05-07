43e542dac2fd9bd9df73c06e2daa0afa8dca3219
Bernardo Estrada <estraadaf.bernardo@gmail.com>
2021-05-06 20:48:42 -0400

    initial

---
+++ ClassExample/ClassExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassExampleApp.swift
+//  ClassExample
+//
+//  Created by user186917 on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ClassExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassExample
+//
+//  Created by user186917 on 5/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
